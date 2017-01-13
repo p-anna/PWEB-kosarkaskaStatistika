11141d7084831cb89192722f7f47ab81e9674cea
p-anna <mn12070@alas.matf.bg.ac.rs>
2017-01-13 18:40:20 +0100

    malo ulepsavanje

--- app/public/app.js
+++ app/public/app.js
@@ -3,23 +3,34 @@ var app = angular.module('app', ["ngRoute"]);
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
         .when('/', {
+            isHome: 'true',
             templateUrl: "templates/home.html",
             controller: 'homeController'
         })
         .when('/players', {
+            isHome: 'false',
             templateUrl: "templates/players.html",
             controller: 'playersController'
         })
         .when('/teams', {
+            isHome: 'false',
             templateUrl: "templates/teams.html",
             controller: 'teamsController'
         })
         .when('/referees', {
+            isHome: 'false',
             templateUrl: "templates/referees.html",
             controller: 'refereesController'
         })
         .when('/games', {
+            isHome: 'false',
             templateUrl: "templates/games.html",
             controller: 'gamesController'
         });
+}]);
+
+app.run(['$rootScope', function($rootScope) {
+    $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+        $rootScope.isHome = current.$$route.isHome;
+    });
 }]);
