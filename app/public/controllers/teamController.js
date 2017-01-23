8fabcd1c98287359be65b072375c27b64c7b3fc9
Ljubica Aćimović <Ljubica Aimovi>
2017-01-23 13:36:59 +0100

    season dropdown

--- app/public/controllers/teamController.js
+++ app/public/controllers/teamController.js
@@ -4,23 +4,29 @@ app.controller('teamController', function($scope, $timeout, $http, $routeParams)
     //za sad se prikazuje jedan zakucan
     $scope.headers = [];
     $scope.games = [];
+    $scope.init = init;
 
     $scope.seasonPart = "Full Season";
+    $scope.seasons = "2016-2017";
 
-    init();
-
-    function init() {
+    $scope.prikazi = function(){
         var season = $scope.seasonPart == "Full Season" ? "null" : $scope.seasonPart;
-
+        var siz = $scope.seasons == "2016-2017" ? 2016 : 2015;
         $http({
             url: "../../source/team.php",
             method: "GET",
-            params: {season: 2016, seasonMonth: season, teamId: $routeParams.id}
+            params: {season: siz, seasonMonth: season, teamId: $routeParams.id}
         }).then(function(response){
             $scope.headers = response.data.header;
             $scope.games = response.data.players;
             $scope.teamName = response.data.teamName;
         });
+    }
+
+    init();
+
+    function init() {
+        $scope.prikazi();
     }
 
     $scope.isNameProp = function (propName) {
